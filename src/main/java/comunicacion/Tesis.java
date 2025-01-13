1897b9247d16ca9d798ea899c11c8affd9b74e2b
sidiazr <sidiazr@unal.edu.co>
2025-01-13 15:11:41 -0500

    Intento 4

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -7,6 +7,18 @@ public class Tesis extends Escrito {
 	private String conclusion;
 	private String referencias;
 	private String interpretacion;
+	
+	public Tesis (String origen, String titulo, String autor, int paginas,
+					String idea, String[] argumentos, String conclusion, 
+					String referencias, String interpretacion) {
+		
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
 
 	@Override
 	int palabrasTotales(int palabrasPagina) {
